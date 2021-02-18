944ecce8c599f24fa0ce6e4f6db37b237e5af0ac
dornkholland <dornkholland@gmail.com>
2021-02-18 10:57:12 -0700

    completed phase 0 of project

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,43 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const csurf = require('csurf');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
-const environment = require('./config');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const { environment } = require("./config");
+const routes = require("./routes");
 
-const isProduction = environment === 'production';
+const isProduction = environment === "production";
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 
 if (!isProduction) {
-    app.use(cors());
+  app.use(cors());
 }
 
-app.use(helmet({
+app.use(
+  helmet({
     contentSecurityPolicy: false,
-}));
+  })
+);
 
-app.use(csurf({
+app.use(
+  csurf({
     cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true,
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
     },
-}));
+  })
+);
+
+// ...
+
+app.use(routes);
+
+module.exports = app;
+
