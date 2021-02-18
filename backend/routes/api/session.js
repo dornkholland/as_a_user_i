c04a92d155d07d36ba8ce50b8c3cf6fc3d6d6acf
dornkholland <dornkholland@gmail.com>
2021-02-18 16:48:01 -0700

    completed testing for backend userauth api routes

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,6 +5,20 @@ const asyncHandler = require("express-async-handler");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
 router.get("/", restoreUser, (req, res) => {
   const { user } = req;
   if (user) {
@@ -17,6 +31,7 @@ router.get("/", restoreUser, (req, res) => {
 // Log in
 router.post(
   "/",
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
