c16ce9d16d8a17298c32c52d9222dc6d260b407b
dornkholland <dornkholland@gmail.com>
2021-02-19 10:10:50 -0700

    completed start of login form component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
+import { Switch, Route } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <div>
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
