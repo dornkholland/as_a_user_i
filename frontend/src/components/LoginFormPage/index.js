7954f2dc90c880d86fbb187ed05edbf2e77f260f
dornkholland <dornkholland@gmail.com>
2021-02-22 13:58:51 -0700

    completed basic styles for user auth

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -2,8 +2,9 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, NavLink } from "react-router-dom";
 import DemoUser from "../DemoUser";
+import Logo from "../Navigation/Logo";
 import "./LoginForm.css";
 
 function LoginFormPage() {
@@ -27,33 +28,46 @@ function LoginFormPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul>
-      <label>
-        Username or Email
-        <input
-          type="text"
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Log In</button>
-      <DemoUser />
-    </form>
+    <div className="loginFormPage">
+      <NavLink to="/" className="logo">
+        <Logo />
+      </NavLink>
+      <h1>Welcome back!</h1>
+      <div className="formContainer">
+        <form className="loginForm" onSubmit={handleSubmit}>
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+          <label>
+            Username or Email
+            <input
+              type="text"
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Log In</button>
+          <h2>
+            First time?
+            <span>
+              <NavLink to="/signup"> Sign up!</NavLink>
+            </span>
+          </h2>
+        </form>
+      </div>
+    </div>
   );
 }
 
