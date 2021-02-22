2d2cf3139f18ca117446f0f41b7f2cc65569ddfb
dornkholland <dornkholland@gmail.com>
2021-02-22 10:37:12 -0700

    completed stylings for hero/landing page

--- frontend/src/components/Landing/Hero.js
+++ frontend/src/components/Landing/Hero.js
@@ -1,3 +1,26 @@
+import { NavLink } from "react-router-dom";
 function Hero() {
-  return <div className="hero"></div>;
+  return (
+    <div className="hero">
+      <div
+        className="hero__image"
+        style={{ backgroundImage: "url(/images/hero.jpg)" }}
+      ></div>
+      <div className="hero__content">
+        <div content__text>
+          <h1>
+            Software should be engineered
+            <span> for the user.</span>
+          </h1>
+          <h2> We can help you with that. </h2>
+        </div>
+        <div className="content__signup">
+          <h1> Sign up within minutes and get started today!</h1>
+          <NavLink to="/signup">Click me!</NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Hero;
